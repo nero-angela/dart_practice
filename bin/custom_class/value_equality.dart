2e814ee3d16a36f09ee58644ba69aa86ad699654
nero <devstory.co.kr@gmail.com>
2023-01-16 18:01:47 +0900

    Implement value_equality.dart.

--- bin/custom_class/value_equality.dart
+++ bin/custom_class/value_equality.dart
@@ -1,9 +1,24 @@
 class A {
-  int value;
+  final int value;
 
-  A(this.value);
+  const A(this.value);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        other is A && runtimeType == other.runtimeType && value == other.value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() {
+    return "A($value)";
+  }
 }
 
 void main() {
   print(A(1) == A(1));
+  print(A(1));
 }
