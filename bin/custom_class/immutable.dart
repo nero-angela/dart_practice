41c75537b47af99cc33017b96601e063d7f7de64
nero <devstory.co.kr@gmail.com>
2023-03-02 13:32:47 +0900

    Add copyWith method.

--- bin/custom_class/immutable.dart
+++ bin/custom_class/immutable.dart
@@ -11,6 +11,16 @@ class A {
   String toString() {
     return "A(value1:$value1, value2:$value2)";
   }
+
+  A copyWith({
+    int? value1,
+    int? value2,
+  }) {
+    return A(
+      value1: value1 ?? this.value1,
+      value2: value2 ?? this.value2,
+    );
+  }
 }
 
 void main() {
@@ -18,7 +28,8 @@ void main() {
   A b = a; // 얕은 복사
 
   // a.value1 = 2; // 에러 발생
-  a = A(value1: 2, value2: a.value2); // 깊은 복사
+  // a = A(value1: 2, value2: a.value2); // 깊은 복사
+  a = a.copyWith(value1: 2); // 깊은 복사
   print(a);
   print(b);
 }
